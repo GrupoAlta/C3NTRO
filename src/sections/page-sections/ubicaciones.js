f052ca0ff1479587b7c10426b3545ca5276bcbd1
Renato Cabrera <renatocabrerafigueroa@gmail.com>
2025-03-11 11:24:43 -0600

    Cambios en front

--- src/sections/page-sections/ubicaciones.js
+++ src/sections/page-sections/ubicaciones.js
@@ -13,8 +13,12 @@ export default function Ubicaciones() {
     <div className="c3ntro-wrapper">
       <div className="c3ntro-wrapper-content sm:mx-1 md:mx-12 lg:mx-12 xl:mx-12 2xl:mx-12">
         <div className="flex items-center justify-center">
-          <h2 className="text-2xl font-bold text-white text-center py-6">NUESTRAS</h2>
-          <h2 className="text-2xl font-bold text-center py-6 px-2" style={{ color: '#CCDEC0'}}>UBICACIONES</h2>
+          <h2 className="nuestras-ubicaciones text-2xl font-bold text-white text-center py-6">
+            NUESTRAS
+          </h2>
+          <h2 className="nuestras-ubicaciones text-2xl font-bold text-center py-6 px-2 text-[#CCDEC0]">
+            UBICACIONES
+          </h2>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
           {/* Ubicación CAES */}
@@ -23,13 +27,23 @@ export default function Ubicaciones() {
               <Image src={marker} alt="location-icon" className="mx-auto" />
               <h3 className="text-xl font-semibold text-c3ntro-blue-secondary mt-4">CAES</h3>
             </div>
-            <p className="text-c3ntro-blue-secondary mb-4">Km 15.5 carretera a El Salvador, Plaza concepción local L29, Nivel 2</p>
-            <div className="bg-card-c3ntro-2-button text-white p-2 rounded-lg flex items-center justify-center gap-1.5">
-              <Image src={whatsapp} alt="whatsapp-icon" className="w-5 h-5"/>
+            <p className="text-c3ntro-blue-secondary mb-4">
+              Km 15.5 carretera a El Salvador, Plaza concepción local L29, Nivel 2
+            </p>
+            <div className="bg-card-c3ntro-2-button text-white p-2 rounded-lg flex items-center justify-center gap-6">
+              <a
+                href={`https://wa.me/37644227?text=${encodeURIComponent("Hola, quisiera más información")}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Image src={whatsapp} alt="whatsapp-icon"  />
+              </a>
               <span className="text-lg font-medium">3764 4227</span>
-              <Image src={arrowRight} alt="call-icon" className="w-5 h-5"/>
+              <a href="tel:37644227">
+                <Image src={arrowRight} alt="call-icon" />
+              </a>
             </div>
-            <Image src={c3ntro2} alt="c3ntro-caes" className="mx-auto mt-4 w-full h-[300px] object-cover rounded-lg"/>
+            <Image src={c3ntro2} alt="c3ntro-caes" className="mx-auto mt-4 w-full h-[300px] object-cover rounded-lg" />
           </div>
 
           {/* Ubicación NOGALES */}
@@ -39,12 +53,20 @@ export default function Ubicaciones() {
               <h3 className="text-xl font-semibold text-white mt-4">NOGALES</h3>
             </div>
             <p className="text-white mb-4">Blvd Rafael Landívar, Nogales de Cayalá, 216. Local 21</p>
-            <div className="bg-card-c3ntro-2-button text-white p-2 rounded-lg flex items-center justify-center gap-1.5">
-              <Image src={whatsapp} alt="whatsapp-icon" className="w-5 h-5"/>
+            <div className="bg-card-c3ntro-2-button text-white p-2 rounded-lg flex items-center justify-center gap-6">
+              <a
+                href={`https://wa.me/35708798?text=${encodeURIComponent("Hola, quisiera más información")}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Image src={whatsapp} alt="whatsapp-icon"  />
+              </a>
               <span className="text-lg font-medium">3570 8798</span>
-              <Image src={arrowRight} alt="call-icon" className="w-5 h-5"/>
+              <a href="tel:35708798">
+                <Image src={arrowRight} alt="call-icon"  />
+              </a>
             </div>
-            <Image src={c3ntro1} alt="c3ntro-nogales" className="mx-auto mt-4 w-full h-[300px] object-cover rounded-lg"/>
+            <Image src={c3ntro1} alt="c3ntro-nogales" className="mx-auto mt-4 w-full h-[300px] object-cover rounded-lg" />
           </div>
 
           {/* Ubicación TIFFANY */}
@@ -53,18 +75,34 @@ export default function Ubicaciones() {
               <Image src={marker} alt="location-icon" className="mx-auto" />
               <h3 className="text-xl font-semibold text-c3ntro-blue-secondary mt-4">TIFFANY</h3>
             </div>
-            <p className="text-c3ntro-blue-secondary mb-4">5 Avenida 3-51, zona 14, Galerías Tiffany, segundo nivel</p>
-            <div className="bg-card-c3ntro-2-button text-white p-2 rounded-lg flex items-center justify-center gap-1.5">
-              <Image src={whatsapp} alt="whatsapp-icon" className="w-5 h-5"/>
+            <p className="text-c3ntro-blue-secondary mb-4">
+              5 Avenida 3-51, zona 14, Galerías Tiffany, segundo nivel
+            </p>
+            <div className="bg-card-c3ntro-2-button text-white p-2 rounded-lg flex items-center justify-center gap-6">
+              <a
+                href={`https://wa.me/35713792?text=${encodeURIComponent("Hola, quisiera más información")}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Image src={whatsapp} alt="whatsapp-icon"  />
+              </a>
               <span className="text-lg font-medium">3571 3792</span>
-              <Image src={arrowRight} alt="call-icon" className="w-5 h-5"/>
+              <a href="tel:35713792">
+                <Image src={arrowRight} alt="call-icon"  />
+              </a>
             </div>
-            <Image src={c3ntro3} alt="c3ntro-tiffany" className="mx-auto mt-4 w-full h-[300px] object-cover rounded-lg"/>
+            <Image src={c3ntro3} alt="c3ntro-tiffany" className="mx-auto mt-4 w-full h-[300px] object-cover rounded-lg" />
           </div>
         </div>
       </div>
       <div className="c3ntro-wrapper-background bg-c3ntro-blue">
-        <Image src={vector} alt="vector" width={"100vw"} height={"100vh"} style={{opacity: 0.2, transform: "rotate(180deg) scale(1.5) translate(-20%, 10%)"}}/>
+        <Image
+          src={vector}
+          alt="vector"
+          width={"100vw"}
+          height={"100vh"}
+          style={{ opacity: 0.2, transform: "rotate(180deg) scale(1.5) translate(-20%, 10%)" }}
+        />
       </div>
     </div>
   );
